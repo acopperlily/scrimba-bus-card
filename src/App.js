292c6c02d18ec3d514dde1d92af83c4905357062
acopperlily <acopperlily@yahoo.com>
2022-11-04 12:35:22 -0400

    Finish business card

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './App.css';
 
 function App() {
   return (
-    <div>
+    <div className='container'>
       <Info />
       <About />
       <Interests />
--- src/components/Info.js
+++ src/components/Info.js
@@ -7,12 +7,14 @@ function Info() {
   return (
     <section className='info'>
       <img src={headshot} alt="april copley" />
-      <h1 className="my-name">April Copley</h1>
-      <h3 className="role">Software Engineer</h3>
-      <span className="portfolio">aprilcopley.netlify.app</span>
-      <div className="contacts">
-        <a className='email' href="http://aprilcopley.netlify.app"><FaEnvelope />Email</a>
-        <a className='linked-in' href="https://www.linkedin.com/in/april-copley/"><FaLinkedin />LinkedIn</a>
+      <div className='details'>
+        <h1 className="my-name">April Copley</h1>
+        <h3 className="role">Software Engineer</h3>
+        <p className="portfolio">aprilcopley.netlify.app</p>
+        <div className="contacts">
+          <button className='email'><FaEnvelope /> Email</button>
+          <button className='linked-in'><FaLinkedin /> LinkedIn</button>
+        </div>
       </div>
     </section>
   );
